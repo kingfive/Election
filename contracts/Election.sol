43a1180dea3203190d72cf2e188c4357c0bf1e36
kingfive <ji3g4zo6qi6@gmail.com>
2018-09-19 12:18:07 +0800

    first commmit

---
+++ contracts/Election.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.24;
+
+contract Election{
+  // store candidate
+  // read candidate
+    string public candidate;
+  // constructor
+    function Election () public{
+        candidate = "MaIn9";
+
+    }
+}
