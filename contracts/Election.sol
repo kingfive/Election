8a6d83ed1632df0a42c51b3e49389e715158ad01
kingfive <ji3g4zo6qi6@gmail.com>
2018-09-26 10:32:15 +0800

    Election

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,12 +1,27 @@
 pragma solidity ^0.4.24;
 
 contract Election{
-  // store candidate
-  // read candidate
-    string public candidate;
-  // constructor
-    function Election () public{
-        candidate = "MaIn9";
+  // model a candidate
+    struct Candidate{
+        uint id;
+        string name;
+        uint voteCount;
+    }
 
+  // store candidate
+  // fetch candidate
+    mapping (uint => Candidate) public candidates;
+
+  // store candidate count
+    uint public candidateCount;
+
+    function Election () public{
+        addCandidate("MaIn9");
+        addCandidate("Vegetable English");
+    }
+
+    function addCandidate(string _name) private {
+        candidateCount++;
+        candidates[candidateCount] = Candidate(candidateCount, _name, 0);
     }
 }
